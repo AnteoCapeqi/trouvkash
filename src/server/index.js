c87c19bdb531c531534b4c559dcd0bda0c3be51c
damien <d.macors@gmail.com>
2019-12-06 15:15:22 +0100

    recuperation de donnee ok

--- src/server/index.js
+++ src/server/index.js
@@ -12,12 +12,50 @@ import path from "path";
 const {APP_PORT} = process.env;
 
 const app = express();
-
+const mongoose = require("mongoose");
+mongoose.set("debug", true);
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
 app.get("/hello", (req, res) => {
     console.log(`ℹ️  (${req.method.toUpperCase()}) ${req.url}`);
     res.send("Hello, World!");
+});
+const Schema = mongoose.Schema;
+
+// eslint-disable-next-line no-unused-vars
+const TerminalSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    bank: Schema.Types.ObjectId,
+    latitude: {type: Number, required: true},
+    longitude: {type: Number, required: true},
+    address: String,
+    created_at: {type: String, required: true},
+    updated_at: {type: String, required: true},
+    deleted_at: {type: String, required: false},
+});
+
+const BankShema = new Schema({
+    country: Number,
+    color: String,
+    name: String,
+    icon: String,
+    url: String,
+    created_at: String,
+    updated_at: String,
+    deleted_at: {type: String, required: false},
+});
+const Banks = mongoose.model("banks", BankShema, "banks");
+
+app.get("/banks", (req, res) => {
+    mongoose.connect("mongodb://dams:dams@mongo/trouvkash", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    // eslint-disable-next-line no-shadow,array-callback-return
+    // eslint-disable-next-line no-shadow,array-callback-return,no-unused-vars
+    Banks.find((err, data) => {
+        res.json(data);
+    });
 });
 
 app.listen(APP_PORT, () =>
