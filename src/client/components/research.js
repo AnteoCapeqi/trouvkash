d4e71940df1ab500bda144ed40a28563bd86d1a2
Surya <hendricks.surya@gmail.com>
2019-12-12 15:33:51 +0100

    change scss

--- src/client/components/research.js
+++ src/client/components/research.js
@@ -1,19 +1,19 @@
 import * as React from "react";
 
-function Research () {
-    
+function Research() {
     return (
         <section>
-            <div id="searchbar">
-                    
-                   
-                <form action="" className="formulaire">
-                    <input className="champ" type="input" ></input>
-                    
+            <div id={"searchbar"}>
+                <form action={""} className={"formulaire"}>
+                    <input
+                        className={"champ"}
+                        placeholder={"    Search a terminals..."}
+                        type={"input"}
+                    />
                 </form>
             </div>
         </section>
-    )
+    );
 }
 
 export default Research;
--- src/client/components/terminals.js
+++ src/client/components/terminals.js
@@ -3,26 +3,27 @@ import axios from "axios";
 
 export default class banks extends React.Component {
     state = {
-      banks: []
-      
-    }
-  
-    componentDidMount() {
-      axios.get(`/terminals`)
-    
-        .then(res => {
-          const banks = res.data;
-          this.setState({ banks });
-        })
-    }
-  
-    render() {
-      return (
-        <ul>
-          { this.state.banks.map(banks => <li>{banks.address} </li>)}
-          
-        </ul>
-      )
-    }
-  }
+        banks: [],
+    };
 
+    componentDidMount() {
+        axios
+            .get(`/terminals`)
+
+            .then(res => {
+                const banks = res.data;
+                this.setState({banks});
+            });
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.state.banks.map(banks => (
+                    <li>{banks.address} </li>
+                    // <li>{"Surya aime les fessée !"} </li>
+                ))}
+            </ul>
+        );
+    }
+}
